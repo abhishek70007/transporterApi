7b0f273de23d920d355cdc652ca4d54c3fe1ec88
mpratham216 <mpratham216@gmail.com>
2023-03-12 12:36:14 +0530

    xml httprequest updates

--- TransporterAPI/src/main/java/com/springboot/TransporterAPI/Controller/TransporterController.java
+++ TransporterAPI/src/main/java/com/springboot/TransporterAPI/Controller/TransporterController.java
@@ -8,14 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.springboot.TransporterAPI.Entity.Transporter;
 import com.springboot.TransporterAPI.Exception.EntityNotFoundException;
@@ -26,6 +19,7 @@ import com.springboot.TransporterAPI.Services.TransporterService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@CrossOrigin
 @RestController
 public class TransporterController {
 
