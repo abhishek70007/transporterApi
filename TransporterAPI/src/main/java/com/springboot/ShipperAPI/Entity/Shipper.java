46f5f1f9dc4d0d22d9b0a39e0db894ce3bb48c3b
mpratham216 <mpratham216@gmail.com>
2023-03-16 17:46:22 +0530

    Revert "email updates"
    This reverts commit 05464831baa8b2aa3d2733727b48485ee5a24cf9.

--- TransporterAPI/src/main/java/com/springboot/ShipperAPI/Entity/Shipper.java
+++ TransporterAPI/src/main/java/com/springboot/ShipperAPI/Entity/Shipper.java
@@ -25,19 +25,11 @@ public class Shipper {
 	private String shipperId;
 	private String shipperName;
 	private String companyName;
-	//@Column(unique=true)
-	//@NotBlank(message = "Phone no. cannot be blank!")
+	@Column(unique=true)
+	@NotBlank(message = "Phone no. cannot be blank!")
 	//	"^[6-9]\\d{9}$", "(^$|[0-9]{10})"
 	@Pattern(regexp="(^[6-9]\\d{9}$)", message="Please enter a valid mobile number") 
 	private String phoneNo;
-
-	@Column(unique=true)
-	@NotBlank(message = "Email can not be blank!")
-	@Pattern(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
-			flags = Pattern.Flag.CASE_INSENSITIVE, message = "Please enter a valid email")
-	private String emailId;
-	private String gst;
-	private String companyStatus;
 	private String kyc;
 	private String shipperLocation;
 
--- TransporterAPI/src/main/java/com/springboot/ShipperAPI/Model/PostShipper.java
+++ TransporterAPI/src/main/java/com/springboot/ShipperAPI/Model/PostShipper.java
@@ -12,14 +12,8 @@ public class PostShipper {
 	private String shipperName;
 	private String companyName;
 	private String shipperLocation;
-	//@NotBlank(message = "Phone no. cannot be blank!")
+	@NotBlank(message = "Phone no. cannot be blank!")
 	@Pattern(regexp="(^[6-9]\\d{9}$)", message="Please enter a valid mobile number") 
 	private String phoneNo;
-	@NotBlank(message = "Email can not be blank!")
-	@Pattern(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
-			flags = Pattern.Flag.CASE_INSENSITIVE, message = "Please enter a valid email")
-	private String emailId;
-	private String gst;
-	private String companyStatus;
 	private String kyc;
 }
