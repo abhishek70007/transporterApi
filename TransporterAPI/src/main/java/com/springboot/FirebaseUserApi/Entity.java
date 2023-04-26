f4bfaef924f713e187eea0717277b6eb39bd2c6e
Jatin Lalwani <112273774+Jatin742@users.noreply.github.com>
2023-04-26 22:42:33 +0530

    EntityNotFoundException Added and Updated Entity

--- TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/Entity.java
+++ TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/Entity.java
@@ -1,12 +1,19 @@
 package com.springboot.FirebaseUserApi;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class Entity {
     private String uid;
     private String email;
-    private String phoneno;
+    private String phoneNo;
+    private String name;
+    private String photoURL;
+    private String providerId;
+    private String tenantId;
+    private boolean isEmailVerified;
 }
