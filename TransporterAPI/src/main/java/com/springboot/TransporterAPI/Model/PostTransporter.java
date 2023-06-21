e5124c726fe4de4d9b970e5373a7c948d9973179
abhishek70007 <132220958+abhishek70007@users.noreply.github.com>
2023-06-21 23:16:26 +0530

    Update PostTransporter.java --emailId added

--- TransporterAPI/src/main/java/com/springboot/TransporterAPI/Model/PostTransporter.java
+++ TransporterAPI/src/main/java/com/springboot/TransporterAPI/Model/PostTransporter.java
@@ -19,5 +19,42 @@ public class PostTransporter {
 	private String transporterName;
 	private String companyName;
 	private String kyc;
+	private String emailId;
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailID(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getTransporterLocation() {
+		return transporterLocation;
+	}
+	public void setTransporterLocation(String transporterLocation) {
+		this.transporterLocation = transporterLocation;
+	}
+	public String getTransporterName() {
+		return transporterName;
+	}
+	public void setTransporterName(String transporterName) {
+		this.transporterName = transporterName;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getKyc() {
+		return kyc;
+	}
+	public void setKyc(String kyc) {
+		this.kyc = kyc;
+	}
 
 }
